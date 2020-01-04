59506acc41eaf7eb7575e56ac9c3cb270ba6116a
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-01-04 23:36:35 +0600

    added login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 import Layout from './components/Layout';
 
 class App extends Component {
 	render() {
 		return (
-			<Layout>
-				<Header>Hi</Header>
-			</Layout>
+			<Switch>
+				<Route exact path='/'>
+					<Login />
+				</Route>
+				<Route exact path='/main'>
+					<Layout>
+						<Header>Hi</Header>
+					</Layout>
+				</Route>
+			</Switch>
 		);
 	}
 }
